b655f637aacdc722f259683c79ff50f4b5153350
Md. Samiul Islam <86099344+samiulislamlive@users.noreply.github.com>
2021-06-21 10:39:48 +0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+	<li class="about">
+					<p>For a Safe Journey</p>
+					<ul>
+						<li><a href="" target="_blank"></a></li>
+						<li><a href="" class="twitter" target="_blank"></a></li>
+						<li><a href="" class="google" target="_blank"></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div class="copyrights wrapper">
+			
+		</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
